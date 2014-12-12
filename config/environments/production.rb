7a51a1b31e842d50f0c02068f2d8f8d5c45832a8
jinxin <238357@gmail.com>
2014-12-12 13:06:32 +0800

    asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,7 @@ Dailyjournal::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.precompile = [/^[-_a-zA-Z0-9]*\..*/]
-
+config.assets.compile = true
   # Generate digests for assets URLs.
   config.assets.digest = true
 
